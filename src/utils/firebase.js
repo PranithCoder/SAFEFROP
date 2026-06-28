49702ca01ed69f1d6472b928f4a65bb1b8d81576
PRANITH SATHIYALINGAM <pranithabde@gmail.com>
2026-06-28 15:08:59 +0530

    Add console.log for firebase API key to debug credentials loading

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -15,6 +15,7 @@ const firebaseConfig = {
 
 
 // Initialize Firebase
+console.log("Firebase API Key in use:", firebaseConfig.apiKey);
 const app = initializeApp(firebaseConfig);
 export const firestore = getFirestore(app);
 export const auth = getAuth(app);
